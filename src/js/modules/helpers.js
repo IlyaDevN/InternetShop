2005a5e6b7439841848ee9a0a7da133a7fe4ae8d
IlyaDevN <nevedrov.87@gmail.com>
2023-10-18 19:52:35 +0300

    refactor: Remove page jump for disableScroll

--- src/js/modules/helpers.js
+++ src/js/modules/helpers.js
@@ -1,7 +1,10 @@
 export function disableScroll() {
+	const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
 	document.body.classList.add("stop-scroll");
+	document.documentElement.style.setProperty("--width-of-scrollbar", scrollBarWidth + "px");
 }
 
 export function enableScroll() {
 	document.body.classList.remove("stop-scroll");
+	document.documentElement.style.setProperty("--width-of-scrollbar", 0);
 }
